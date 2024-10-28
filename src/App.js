d8592bca19b5ac81a2d90f2869559bbbf69fec3d
Konnichiwaitsnahoko <154089390+Konnichiwaitsnahoko@users.noreply.github.com>
2024-10-28 13:47:35 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Hello</header>
+      <button type="button" class="btn btn-outline-primary">
+        Primary
+      </button>
+    </div>
+  );
+}
