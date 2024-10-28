0ddee0a50a217e09928032be23b5584d5a529b75
Konnichiwaitsnahoko <154089390+Konnichiwaitsnahoko@users.noreply.github.com>
2024-10-28 14:41:38 -0700

    Added footer and made a new component "Dictionary"

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">Hello</header>
-      <button type="button" class="btn btn-outline-primary">
-        Primary
-      </button>
+      <div className="container">
+        <header className="App-header">Dictionary App</header>
+
+        <main>
+          <Dictionary />
+        </main>
+
+        <footer className="text-center">
+          This project is coded by{" "}
+          <a href="https://github.com/Konnichiwaitsnahoko" target="_blank">
+            Nahoko Park
+          </a>{" "}
+          and is open-sourced on{" "}
+          <a
+            href="https://github.com/Konnichiwaitsnahoko/dictionary-app"
+            target="_blank">
+            GitHub
+          </a>{" "}
+          and hosted on{" "}
+          <a href="https://dictionary-jisho.netlify.app/" target="_blank">
+            Netlify
+          </a>
+          .
+        </footer>
+      </div>
     </div>
   );
 }
