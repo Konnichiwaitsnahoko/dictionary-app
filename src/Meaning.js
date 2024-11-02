8584f7f7777840f3a38885daa0aecc2a4ff45a5b
Konnichiwaitsnahoko <154089390+Konnichiwaitsnahoko@users.noreply.github.com>
2024-11-02 14:36:46 -0700

    Display a max of three definitions and made labeling

--- src/Meaning.js
+++ src/Meaning.js
@@ -5,19 +5,23 @@ export default function Meaning(props) {
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
-      {props.meaning.definitions.map(function (definition, index) {
-        return (
-          <div key={index}>
-            <p>
-              {" "}
-              <strong>Definition:</strong> {definition.definition}
-              <br></br>
-              <strong>Example: </strong>
-              {definition.example}
-            </p>
-          </div>
-        );
-      })}
+      <ol>
+        {props.meaning.definitions
+          .slice(0, 3)
+          .map(function (definition, index) {
+            return (
+              <li key={index}>
+                <strong>Definition: </strong>
+                {definition.definition}
+                {definition.example && (
+                  <p>
+                    <strong>Example: </strong> {definition.example}
+                  </p>
+                )}
+              </li>
+            );
+          })}
+      </ol>
       <Synonyms synonyms={props.meaning.synonyms} />
     </div>
   );
